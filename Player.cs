6dd839ae06f37021296146095a9ec8542d87414a
Kyle <misenckj@gmail.com>
2024-05-18 14:42:08 -0600

    Add cooldown to shooting input

--- Player.cs
+++ Player.cs
@@ -13,10 +13,15 @@ public partial class Player : Area2D
 
 	public Vector2 ScreenSize; // Size of the game window.
 
+	private float ShootCooldown = 1.0f;
+	private float _timeSinceLastShot = 0f;
+	
+
 	private void OnBodyEntered(Node2D body)
 	{
 		GD.Print("Player Hit!");
 		// TODO: Trigger way to flash Player sprite
+		// TODO: Remove Health
 		EmitSignal(SignalName.Hit);
 		GetNode<CollisionPolygon2D>("CollisionPolygon2D").SetDeferred(CollisionPolygon2D.PropertyName.Disabled, true);
 	}
@@ -44,6 +49,7 @@ public partial class Player : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		_timeSinceLastShot += (float)delta;
 		Vector2 velocity = Vector2.Zero;
 
 		if (Input.IsActionPressed("player_left"))
@@ -54,9 +60,10 @@ public partial class Player : Area2D
 		{
 			velocity.X += 1;
 		}
-		if (Input.IsActionPressed("player_shoot"))
+		if (Input.IsActionPressed("player_shoot") && _timeSinceLastShot >= ShootCooldown)
 		{
 			Shoot();
+			_timeSinceLastShot = 0;
 		}
 
 		if (velocity.Length() > 0)
