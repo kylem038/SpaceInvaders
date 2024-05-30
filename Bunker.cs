e4259528cf68b0cdff67b48596900940ec5b04a3
Kyle <misenckj@gmail.com>
2024-05-29 21:35:42 -0600

    Bunker animation working :)

--- Bunker.cs
+++ Bunker.cs
@@ -3,16 +3,31 @@ using System;
 
 public partial class Bunker : Area2D
 {
-	private int _health = 4;
+	private int _health = 3;
+
+	AnimatedSprite2D animation { get; set; }
 
 	private void OnBodyEntered(Node2D body)
 	{
+		_health -= 1;
+		if (_health == 0)
+		{
+			GD.Print("Clearing Bunker");
+			QueueFree();
+		}
+		else
+		{
+			animation.Frame += 1;
+		}
+		
 		body.QueueFree();
 	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		animation = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animation.Frame = 0;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
