46223356336af1de353b3f9fba878d5e9cf9fc4d
Kyle <misenckj@gmail.com>
2024-06-02 12:37:34 -0600

    Add explosion for mothership collision

--- Mothership.cs
+++ Mothership.cs
@@ -11,15 +11,44 @@ public partial class Mothership : Area2D
 
 	[Export]
 	public PackedScene LasersScene { get; set; }
+
+	[Export]
+	public PackedScene ExplosionScene { get; set; }
+
 	PathFollow2D Pathing { get; set; }
+	private Timer _explosionTimer;
 	private int lasersVelocity = 400;
+
+	private void TriggerExplosion()
+	{
+		Node2D explosionScene = (Node2D)ExplosionScene.Instantiate();
+		AddChild(explosionScene);
+		CpuParticles2D explosion = GetNode<CpuParticles2D>("Explosion/CPUParticles2D");
+		explosion.ScaleAmountMin = explosion.ScaleAmountMin * 2;
+		explosion.ScaleAmountMax = explosion.ScaleAmountMax * 2;
+		explosion.Amount = 40;
+		explosion.Emitting = true;
+		_explosionTimer.Start();
+	}
+
+	private void OnExplosionTimerTimeout()
+	{
+		CpuParticles2D explosion = GetNode<CpuParticles2D>("Explosion/CPUParticles2D");
+		GD.Print("Are we still emitting? ", explosion.Emitting);
+		if (!explosion.Emitting)
+		{
+			// Remove Mothership from screen after particles are done
+			QueueFree();
+		}
+	}
 
 	private void OnBodyEntered(Node2D body)
 	{
 		GetNode<Timer>("/root/Main/MothershipSpawnTimer").Start();
 		EmitSignal(SignalName.UpdateScore, 50);
 		body.QueueFree();
-		QueueFree();
+		TriggerExplosion();
+		// QueueFree();
 	}
 
 	private void OnShootTimerTimeout()
@@ -37,12 +66,15 @@ public partial class Mothership : Area2D
 		Pathing = (PathFollow2D)GetParent();
 		GetNode<Timer>("ShootTimer").Start();
 		UpdateScore += GetNode<Main>("/root/Main").UpdateScore;
+		_explosionTimer = GetNode<Timer>("ExplosionTimer");
+		_explosionTimer.Timeout += OnExplosionTimerTimeout;
 	}
 
 	// Called when the node is about to exit the scene tree
     public override void _ExitTree()
     {
 		UpdateScore -= GetNode<Main>("/root/Main").UpdateScore;
+		_explosionTimer.Timeout -= OnExplosionTimerTimeout;
         base._ExitTree();
     }
 
